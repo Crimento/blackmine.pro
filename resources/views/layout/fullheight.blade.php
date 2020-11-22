a039f6176641adbbb58ada98d044765030155232
Alexander Abramenko <crimento@gmail.com>
2020-11-22 22:41:40 +0300

    no fixed background  :(

--- resources/views/layout/fullheight.blade.php
+++ resources/views/layout/fullheight.blade.php
@@ -2,7 +2,7 @@
 
 @section('skeleton-content')
 <div class="container-fluid d-flex flex-column h-100 p-0">
-    <div id="content" class="bg-dark flex-grow-1">
+    <div id="content" class="bg-dark flex-fill">
         @yield('content')
     </div>
     @include('layout.footer')
